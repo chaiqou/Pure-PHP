3bfb35a9912e6ba65b687b9b103dc700ec063db1
chaiqou <lomtadzenikusha@gmail.com>
2022-09-17 16:23:04 +0400

    feat: create addDiscount and taxes methods also calculate amount and create getAmount method

--- Transaction.php
+++ Transaction.php
@@ -2,14 +2,38 @@
 
 declare(strict_types=1);
 
-class Transaction // this is class
+// this is class
+
+class Transaction
 {
-    public float $amount;
-    public string $description;
+    private float $amount;
+    private string $description;
 
     public function __construct(float $amount, string $description)
     {
         $this->amount = $amount;
         $this->description = $description;
+    }
+
+    // Calculate tax and amount together
+    public function addTax(float $rate): Transaction
+    {
+        $this->amount = $this->amount + ($this->amount * $rate) / 100;
+
+        return $this;
+    }
+
+    // Calculate discount and amount together
+    public function addDiscount(float $rate): Transaction
+    {
+        $this->amount = $this->amount - ($this->amount * $rate) / 100;
+
+        return $this;
+    }
+
+    // get amount outside class scope
+    public function getAmount(): float
+    {
+        return $this->amount;
     }
 }
--- index.php
+++ index.php
@@ -2,8 +2,10 @@
 
 require_once "Transaction.php";
 
-$transaction = new Transaction(); // this is object
+// This is a Object class instance
+// Chanining რომ იყოს შესაძლებელი მეთოდიდან უნდა დავაბრუნოთ კლასის ინსტანსი ანუ $this ,ასევე new ინსტანსი უნდა ჩავსვათ ფრჩხილებში
+$transaction = (new Transaction(100, "new transaction 1"))
+    ->addTax(8)
+    ->addDiscount(10);
 
-$transaction->amount = 20;
-
-var_dump($transaction->amount);
+var_dump($transaction->getAmount());

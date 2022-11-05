dfda20525157f0eab3cc8ee2bc64ace48e94a6ac
chaiqou <lomtadzenikusha@gmail.com>
2022-11-06 02:35:28 +0400

    feat: PDO

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php 
 
 require 'functions.php';
-require 'router.php';
+// require 'router.php';
+
+// connect to database
+
+$dsn = "mysql:host=localhost;port=3306;dbname=pure_php;charset=utf8mb4";
+
+$pdo = new PDO($dsn, 'username', 'password');
+
+$statement = $pdo->prepare("SELECT * FROM posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+dd($posts);
 

16534f10181f954742e5b9cb0c99233a9624a048
chaiqou <lomtadzenikusha@gmail.com>
2022-11-19 16:15:03 +0400

    feat: Add database file

--- index.php
+++ index.php
@@ -1,18 +1,10 @@
-<?php 
+<?php
 
 require 'functions.php';
-// require 'router.php';
+//require 'router.php';
+require 'Database.php';
 
-// connect to database
-
-$dsn = "mysql:host=localhost;port=3306;dbname=pure_php;charset=utf8mb4";
-
-$pdo = new PDO($dsn, 'username', 'password');
-
-$statement = $pdo->prepare("SELECT * FROM posts");
-$statement->execute();
-
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+$db = new Database();
+$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
 
 dd($posts);
-
